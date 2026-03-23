bd3c0184d9b93121e7f2da9f28cdc880aed9bdae
Terry Tao <librakevin@gmail.com>
2026-03-23 09:21:45 -0400

    some updates

--- wezterm/.config/wezterm/appearance.lua
+++ wezterm/.config/wezterm/appearance.lua
@@ -7,6 +7,10 @@ local module = {}
 function module.setup(config)
     config.initial_cols = 140
     config.initial_rows = 30
+    config.enable_scroll_bar = true
+    config.colors = {
+        scrollbar_thumb = "#00ffff",
+    }
 end
 
 return module
